edf967a1ea966b24dc3aefee225f9fae01e08d30
abhiyanpoudel <getme_max@hotmail.com>
2018-07-31 07:50:27 +0545

    Initial commit

---
+++ app/src/main/java/com/example/getme/fullscreen/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.getme.fullscreen;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Typeface mytype = Typeface.createFromAsset(getAssets(), "Ginga.ttf");
+        TextView mytext = (TextView)findViewById(R.id.textview1);
+        mytext.setTypeface(mytype);
+
+    }
+}
